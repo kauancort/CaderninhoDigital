730f94293604b0abedd366822c549098148bf4a5
kauan <kauancortico33@gmail.com>
2026-07-04 10:24:42 -0300

    primeiro commit backend

---
+++ springBoot/src/main/java/com/InovaSkill/CaderninhoDigital/CaderninhoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.InovaSkill.CaderninhoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaderninhoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaderninhoDigitalApplication.class, args);
+	}
+
+}
---
+++ springBoot/src/test/java/com/InovaSkill/CaderninhoDigital/CaderninhoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InovaSkill.CaderninhoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaderninhoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
